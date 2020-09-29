e9145d405fe85a1d8310ec3f8493c432b4150d90
Miguel Ángel Fernández Carrillo <migferna@student.42madrid.com>
2020-09-29 13:14:12 +0200

    Caracteres especiales tomados como literales

--- srcs/ft_echo.c
+++ srcs/ft_echo.c
@@ -6,17 +6,58 @@
 /*   By: migferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/12 10:03:29 by migferna          #+#    #+#             */
-/*   Updated: 2020/09/18 09:21:27 by vde-dios         ###   ########.fr       */
+/*   Updated: 2020/09/29 13:13:13 by migferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	find_all_characters(char* str, char c)
+{
+	size_t	len;
+	size_t	cont;	
+
+	cont = 0;
+	len = 0;
+	while (*str)
+	{
+		if (*str == c)
+			cont++;
+		str++;
+	}
+	return (cont);
+	
+}
+
+static char	*clean(char *str, char *blacklist)
+{
+	char	*str_clean;
+	char	*tmp;
+	size_t	len_orig;
+	size_t	cont;
+	
+	len_orig = ft_strlen(str);
+	cont = find_all_characters(str, *blacklist);
+	str_clean = ft_calloc(sizeof(char), len_orig - cont);
+	tmp = str_clean;
+	while (*str)
+	{
+		if (*str != *blacklist)
+		{
+			*str_clean = *str;
+			str_clean++;
+		}
+		str++;
+	}
+	return (tmp);
+}
 
 int		ft_echo(char **args)
 {
 	int		end;
 	size_t	len;
 	size_t	it;
+	char	*tmp;
 
 	it = 0;
 	end = 1;
@@ -24,11 +65,14 @@ int		ft_echo(char **args)
 		end = 0;
 	while (*args)
 	{
-		len = ft_strlen(*args);
-		write(1, *args++, len);
+		tmp = clean(*args, "\\");
+		len = ft_strlen(tmp);
+		write(1, tmp, len);
+		args++;
 		if (*args)
 			write(1, " ", 1);
 	}
+	free(tmp);
 	if (end)
 		write(1, "\n", 1);
 	return (1);
