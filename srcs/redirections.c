69fdcc36266ec427f017ca73173b0af9a8fd565e
Miguel Ángel Fernández Carrillo <migferna@student.42madrid.com>
2020-10-10 18:13:15 +0200

    Redirections fixed bash

--- srcs/redirections.c
+++ srcs/redirections.c
@@ -14,7 +14,7 @@ static	int	redirections_append(t_shell *shell, size_t it)
 static int	redirections_output(t_shell *shell, size_t it)
 {	
 	int fd;
-		
+	
 	fd = open(shell->args[it + 1], O_CREAT | O_TRUNC | O_WRONLY, 0644);
 	ft_unset(shell->args[it], shell->args);
 	ft_unset(shell->args[it], shell->args);
@@ -38,8 +38,8 @@ int	find_redirections(t_shell *shell)
 	size_t 	it;
 	int		fd;
 	
-	it = -1;
-	while (shell->args[++it])
+	it = 0;
+	while (shell->args[it])
 	{
 		if (ft_strcmp(shell->args[it], ">"))
 			fd = redirections_output(shell, it);
@@ -49,6 +49,8 @@ int	find_redirections(t_shell *shell)
 			fd = redirections_append(shell, it);
 		//else if (ft_strcmp(shell->arg, "|"))
 		//	redirection_pipe();	
+		else
+			it++;
 	}
 	return (fd);
 }
