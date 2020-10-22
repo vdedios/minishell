dd9e02942cb94fc5df4859e8610cb00842e4ccea
Miguel Ángel Fernández Carrillo <migferna@student.42madrid.com>
2020-10-22 21:30:09 +0200

    Ansi Code Control-C

--- srcs/signals.c
+++ srcs/signals.c
@@ -6,7 +6,7 @@
 /*   By: migferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/12 11:17:38 by migferna          #+#    #+#             */
-/*   Updated: 2020/09/17 11:21:00 by vde-dios         ###   ########.fr       */
+/*   Updated: 2020/10/22 21:00:55 by migferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	signal_handler_running(int sig)
 {
 	if (sig == SIGINT)
-		ft_putstr_fd("\n$:\\>", 1);
+		ft_putstr_fd("\033[2D\033[J\n$:\\>", 1);
 	signal(SIGINT, signal_handler_running);
 	signal(SIGQUIT, signal_handler_running);
 }
