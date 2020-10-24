1ccca20b5353405ba4d0d90d253fad0d507e5a7d
Miguel Ángel Fernández Carrillo <migferna@students.42madrid.com>
2020-10-24 18:43:47 +0200

    Control + \ fixed

--- srcs/signals.c
+++ srcs/signals.c
@@ -6,7 +6,7 @@
 /*   By: migferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/12 11:17:38 by migferna          #+#    #+#             */
-/*   Updated: 2020/10/22 21:00:55 by migferna         ###   ########.fr       */
+/*   Updated: 2020/10/24 18:32:32 by migferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,15 @@
 void	signal_handler_running(int sig)
 {
 	if (sig == SIGINT)
+	{
 		ft_putstr_fd("\033[2D\033[J\n$:\\>", 1);
-	signal(SIGINT, signal_handler_running);
-	signal(SIGQUIT, signal_handler_running);
+		signal(SIGINT, signal_handler_running);
+	}
+	else if (sig == SIGQUIT)
+	{
+		ft_putstr_fd("\033[2D\033[J", 1);
+		signal(SIGQUIT, signal_handler_running);
+	}
 }
 
 void	signal_handler_waiting(int sig)
