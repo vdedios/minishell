b57770dd01fbe500b23d469ffedfa37372b71748
Miguel Ángel Fernández Carrillo <migferna@students.42madrid.com>
2020-11-28 12:01:40 +0100

    Test: ft_exit

--- srcs/ft_exit.c
+++ srcs/ft_exit.c
@@ -6,22 +6,53 @@
 /*   By: migferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/12 13:17:27 by migferna          #+#    #+#             */
-/*   Updated: 2020/11/23 19:21:34 by migferna         ###   ########.fr       */
+/*   Updated: 2020/11/28 11:58:47 by migferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	ft_iterate(const char *nptr, long long *num)
+{
+	while (*nptr >= '0' && *nptr <= '9')
+	{
+		*num = (*num) * 10 + *nptr - '0';
+		nptr++;
+	}
+}
+
+static	int		exit_atoi(const char *nptr)
+{
+	long long	num;
+	int		sign;
+
+	sign = 0;
+	num = 0;
+	while (*nptr == '\t' || *nptr == '\n' || *nptr == '\v'
+		|| *nptr == '\f' || *nptr == '\r' || *nptr == ' ')
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
+	{
+		if (*nptr == '-')
+			sign = 1;
+		nptr++;
+	}
+	ft_iterate(nptr, &num);
+	if (sign == 1)
+		num = -1 * num;
+	return ((int)num);
+}
 void	ft_exit(t_shell *shell)
 {
-	int value;
+	int 	value;
+	char	*nbr;
 
-	if (!shell->args[1])
+	nbr = shell->args[1];
+	if (!nbr)
 		value = 0;
 	else
-		value = ft_atoi(shell->args[1]);
+		value = exit_atoi(nbr);
 	shell->stat_loc = value;
 	clean_shell(shell);
-	//kill(0, SIGTERM);
 	exit(value);
 }
