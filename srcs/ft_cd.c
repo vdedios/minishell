665d62e4e286c40f4e6519bdd31f3d068398d7d5
Miguel Ángel Fernández Carrillo <migferna@students.42madrid.com>
2020-12-13 13:28:17 +0100

    Fix cd

--- srcs/ft_cd.c
+++ srcs/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: migferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/12 10:58:41 by migferna          #+#    #+#             */
-/*   Updated: 2020/12/13 01:04:58 by migferna         ###   ########.fr       */
+/*   Updated: 2020/12/13 13:25:31 by migferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,22 @@ static void	set_path(char *path, const char *key, t_shell *shell)
 		}
 		it++;
 	}
-	free(path);
+	//free(path);
+}
+
+static char	*get_path(t_shell *shell, char *path)
+{
+	size_t 	it;
+	size_t	len;
+
+	it = -1;
+	len = ft_strlen(path);
+	while (shell->env[++it])
+	{
+		if (!ft_strncmp(shell->env[it], path, len))
+			return (ft_strchr(shell->env[it], '=') + 1);
+	}
+	return (NULL);
 }
 
 static void	change_dir(char *path, t_shell *shell)
@@ -45,11 +60,14 @@ static void	change_dir(char *path, t_shell *shell)
 	oldcwd = ft_calloc(1024, sizeof(oldcwd));
 	cwd = ft_calloc(1024, sizeof(cwd));
 	getcwd(oldcwd, 1024);
+	//oldcwd = get_path(shell, "PWD=");
+	//printf("Sale: %s\n", oldcwd);
+	//set_path(oldcwd, "PWD=", shell);
 	if (chdir(path) == 0)
 	{
 		getcwd(cwd, 1024);
+		set_path(get_path(shell, "PWD="), "OLDPWD=", shell);
 		set_path(cwd, "PWD=", shell);
-		set_path(oldcwd, "OLDPWD=", shell);
 	}
 	else
 	{
