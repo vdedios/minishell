5291ed697f39bfd7140714ea73a0a0be2f83c74d
Victor <victor.dedios@outlook.com>
2021-02-18 19:05:14 +0100

    check permissions to set_err_message

--- srcs/permissions.c
+++ srcs/permissions.c
@@ -12,45 +12,27 @@
 
 #include "minishell.h"
 
+static void	set_err_message(t_shell *shell, char *err, int code)
+{
+	shell->stat_loc = code;
+	print_errors(shell, err, shell->binary);
+	exit(shell->stat_loc);
+}
+
 void	check_permissions(t_shell *shell, char *path, int *binary)
 {
 	struct stat s;
 
 	if (stat(path, &s) != -1)
 	{
-		//printf("Path: %s\n", path);
 		if (s.st_mode & S_IFDIR)
-		{
-			shell->stat_loc = 126;
-			print_errors(shell, " is a directory", shell->binary);
-			exit(shell->stat_loc);
-		}
-
+			set_err_message(shell, " is a directory", 126);
 		if (*binary == 1)
-		{
-			if (!(s.st_mode & S_IXUSR))
-			{
-				shell->stat_loc = 126;
-				print_errors(shell, " Permission denied", path);
-				exit(shell->stat_loc);
-			}
-		}
-		else if (*binary == 0)
-		{
-			if (!(s.st_mode & S_IXUSR) || !(s.st_mode & S_IRUSR))
-			{
-				//printf("Usuario");
-				shell->stat_loc = 126;
-				print_errors(shell, " Permission denied", path);
-				exit(shell->stat_loc);
-			}
-		}
-		
+			set_err_message(shell, " Permission denied", 126);
+		else if (*binary == 0 &&
+				(!(s.st_mode & S_IXUSR) || !(s.st_mode & S_IRUSR)))
+				set_err_message(shell, " Permission denied", 126);
 	}
 	else
-	{
-		shell->stat_loc = 127;
-		print_errors(shell, " command not found", shell->binary);
-		exit(shell->stat_loc);
-	}
+		set_err_message(shell, " command not found", 127);
 }
