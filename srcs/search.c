950d9c3884aebb14281b2cdce377f205ddfb37ff
Victor <victor.dedios@outlook.com>
2021-02-18 21:57:40 +0100

    search_binary to binary_path_exists

--- srcs/search.c
+++ srcs/search.c
@@ -25,69 +25,72 @@ static	void	to_lower(char *input)
 
 char	*search_binary_in_pwd(t_shell *shell)
 {
-	//char			cwd[1024];
 	struct stat		s;
 	struct dirent	*direntp;
 	DIR				*pdir;
 
-//	if (getcwd(cwd, sizeof(cwd)))
-//	{
-		if (stat(".", &s) != -1 &&
-			s.st_mode & S_IFDIR)
+	if (stat(".", &s) != -1 &&
+		s.st_mode & S_IFDIR)
+	{
+		if (!(pdir = opendir(".")))
+			return (NULL);
+		while ((direntp = readdir(pdir)))
 		{
-			if (!(pdir = opendir(".")))
-				return (NULL);
-			while ((direntp = readdir(pdir)))
+			if (ft_strcmp(direntp->d_name, shell->args[0]))
 			{
-				if (ft_strcmp(direntp->d_name, shell->args[0]))
-				{
-					closedir(pdir);
-					return (absolute_bin_path(".", shell->binary));
-				}
-				direntp++;
+				closedir(pdir);
+				return (absolute_bin_path(".", shell->binary));
 			}
-			closedir(pdir);
+			direntp++;
 		}
-		shell->stat_loc = 127;
-		print_errors(shell, " No such file or directory", shell->binary);
-		exit(shell->stat_loc);
-//	}
+		closedir(pdir);
+	}
+	shell->stat_loc = 127;
+	print_errors(shell, " No such file or directory", shell->binary);
+	exit(shell->stat_loc);
 	return (NULL);
+}
+
+static	short	binary_path_exists(char *path,char *bin_name,
+									int *binary, struct stat *s)
+{
+	DIR				*pdir;
+	struct dirent	*direntp;
+
+	if (stat(path, s) != -1 &&
+		s->st_mode & S_IFDIR)
+	{
+		if (!(pdir = opendir(path)))
+			return (0);
+		while ((direntp = readdir(pdir)))
+		{
+			if (ft_strcmp(direntp->d_name, bin_name))
+			{
+				if (binary)
+					*binary = 1;
+				closedir(pdir);
+				free(bin_name);
+				return (1);
+			}
+			direntp++;
+		}
+		closedir(pdir);
+	}
+	return (0);
 }
 
 char	*search_binary(t_shell *shell, char **paths, int *binary)
 {
 	size_t			it;
 	struct stat		s;
-	struct dirent	*direntp;
-	DIR				*pdir;
 	char			*bin_name;
 
 	it = -1;
 	bin_name = ft_strdup(shell->args[0]);
 	to_lower(bin_name);
 	while (paths[++it])
-	{
-		if (stat(paths[it], &s) != -1 &&
-			s.st_mode & S_IFDIR)
-		{
-			if (!(pdir = opendir(paths[it])))
-				return (NULL);
-			while ((direntp = readdir(pdir)))
-			{
-				if (ft_strcmp(direntp->d_name, bin_name))
-				{
-					if (binary)
-						*binary = 1;
-					closedir(pdir);
-					free(bin_name);
-					return (absolute_bin_path(paths[it], shell->binary));
-				}
-				direntp++;
-			}
-			closedir(pdir);
-		}
-	}
+		if (binary_path_exists(paths[it], bin_name, binary, &s))
+			return (absolute_bin_path(paths[it], shell->binary));
 	if (stat(bin_name, &s) != -1)
 	{
 		if (S_ISDIR(s.st_mode) || S_ISREG(s.st_mode))
