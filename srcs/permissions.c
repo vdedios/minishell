e73b45f175a719557861674b411e0ee64a08cfb3
Victor <victor.dedios@outlook.com>
2021-02-18 19:15:32 +0100

    set_err_message: fix path

--- srcs/permissions.c
+++ srcs/permissions.c
@@ -12,10 +12,10 @@
 
 #include "minishell.h"
 
-static void	set_err_message(t_shell *shell, char *err, int code)
+static void	set_err_message(t_shell *shell, char *err, int code, char *arg)
 {
 	shell->stat_loc = code;
-	print_errors(shell, err, shell->binary);
+	print_errors(shell, err, arg);
 	exit(shell->stat_loc);
 }
 
@@ -26,13 +26,13 @@ void	check_permissions(t_shell *shell, char *path, int *binary)
 	if (stat(path, &s) != -1)
 	{
 		if (s.st_mode & S_IFDIR)
-			set_err_message(shell, " is a directory", 126);
+			set_err_message(shell, " is a directory", 126, shell->binary);
 		if (*binary == 1 && (!(s.st_mode & S_IXUSR)))
-			set_err_message(shell, " Permission denied", 126);
+			set_err_message(shell, " Permission denied", 126, path);
 		else if (*binary == 0 &&
 				(!(s.st_mode & S_IXUSR) || !(s.st_mode & S_IRUSR)))
-				set_err_message(shell, " Permission denied", 126);
+				set_err_message(shell, " Permission denied", 126, path);
 	}
 	else
-		set_err_message(shell, " command not found", 127);
+		set_err_message(shell, " command not found", 127, shell->binary);
 }
