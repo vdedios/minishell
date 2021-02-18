1e2e90e97b754ea17063c7d3a9095354a31f0be4
Victor <victor.dedios@outlook.com>
2021-02-18 20:04:42 +0100

    find_pipes to child_pipe

--- srcs/pipes.c
+++ srcs/pipes.c
@@ -38,45 +38,48 @@ static void		exec_arg(t_shell *shell)
 	}
 }
 
+static int		child_pipe(t_shell *shell, int it, int p[2])
+{
+	pid_t	pid;
 
-void	find_pipes(t_shell *shell)
+	if ((pid = fork()) == 0)
+	{
+		close(p[0]);
+		if (shell->commands[it + 1])
+			dup2(p[1], 1);
+		find_redirections(shell);
+		exec_arg(shell);
+		close(p[1]);
+		exit(shell->stat_loc);
+	}
+	return (pid);
+}
+
+void			find_pipes(t_shell *shell)
 {
 	pid_t	pid;
 	pid_t	aux_pid;
 	int		p[2];
 	int		it;
 
-	it = 0;
-	while (shell->commands[it])
+	it = -1;
+	while (shell->commands[++it])
 	{
 		get_args_and_binary(shell, it);
 		pipe(p);
-		if ((pid = fork()) == 0)
-		{
-			close(p[0]);
-			if (shell->commands[it + 1])
-				dup2(p[1], 1);
-			find_redirections(shell);
-			exec_arg(shell);
-			close(p[1]);
-			exit(shell->stat_loc);
-		}
+		pid = child_pipe(shell, it, p);
 		close(p[1]);
 		dup2(p[0], 0);
-		it++;
 		if (shell->commands[it])
 			waitpid(pid, &shell->stat_loc, WNOHANG);
 		shell->stat_loc = WEXITSTATUS(shell->stat_loc);
 		close(p[0]);
-
 		clean_matrix(shell->args);
 		free(shell->args);
 		free(shell->binary);
 	}
 	while ( (aux_pid = wait(&shell->stat_loc)) > 0)
-	{
 		if (aux_pid < pid)
 			shell->stat_loc = WEXITSTATUS(shell->stat_loc);
-	}
 	shell->stat_loc = WEXITSTATUS(shell->stat_loc);
 }
