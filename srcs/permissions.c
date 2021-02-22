4f0969d2e823aece65165f7dc3a1e60898e71464
Victor <victor.dedios@outlook.com>
2021-02-22 17:16:33 +0100

    fix permssions

--- srcs/permissions.c
+++ srcs/permissions.c
@@ -12,10 +12,11 @@
 
 #include "minishell.h"
 
-static void	set_err_message(t_shell *shell, char *err, int code, char *arg)
+static int	set_err_message(t_shell *shell, char *err, int code, char *arg)
 {
 	shell->stat_loc = code;
 	print_errors(shell, err, arg);
+	return (0);
 }
 
 int			check_permissions(t_shell *shell, char *path, int *binary)
@@ -25,26 +26,14 @@ int			check_permissions(t_shell *shell, char *path, int *binary)
 	if (stat(path, &s) != -1)
 	{
 		if (s.st_mode & S_IFDIR)
-		{
-			set_err_message(shell, " is a directory", 126, shell->binary);
-			return (0);
-		}
+			return (set_err_message(shell, " is a directory", 126, shell->binary));
 		if (*binary == 1 && (!(s.st_mode & S_IXUSR)))
-		{
-			set_err_message(shell, " Permission denied", 126, path);
-			return (0);
-		}
+			return (set_err_message(shell, " Permission denied", 126, path));
 		else if (*binary == 0 &&
 				(!(s.st_mode & S_IXUSR) || !(s.st_mode & S_IRUSR)))
-		{
-			set_err_message(shell, " Permission denied", 126, path);
-			return (0);
-		}
+			return (set_err_message(shell, " Permission denied", 126, path));
 		return (1);
 	}
 	else
-	{
-		set_err_message(shell, " command not found", 127, shell->binary);
-		return (0);
-	}
+		return (set_err_message(shell, " command not found", 127, shell->binary));
 }
