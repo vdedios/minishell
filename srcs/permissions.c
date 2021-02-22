2bac2aa8079679ec39fe1949f79816b163a6ffb8
Miguel Ángel Fernández Carrillo <migferna@student.42madrid.com>
2021-02-22 18:22:52 +0100

    fix normi

--- srcs/permissions.c
+++ srcs/permissions.c
@@ -6,7 +6,7 @@
 /*   By: migferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/07 12:53:16 by migferna          #+#    #+#             */
-/*   Updated: 2021/02/21 00:15:39 by migferna         ###   ########.fr       */
+/*   Updated: 2021/02/22 18:21:55 by migferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int			check_permissions(t_shell *shell, char *path)
 	{
 		if (s.st_mode & S_IFDIR)
 			return (set_err_message(shell, " is a directory", 126
-					, shell->binary));
+						, shell->binary));
 		if (shell->is_binary == 1 && (!(s.st_mode & S_IXUSR)))
 			return (set_err_message(shell, " Permission denied", 126, path));
 		else if (shell->is_binary == 0 &&
